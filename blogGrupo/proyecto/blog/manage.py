2ba65d9263e46f699fbc5c300887b5947f869006
Gerardo Juan Martin Serrano Gallego <98660245+serranogallegogerardo@users.noreply.github.com>
2023-11-28 18:04:59 -0300

    Update manage.py

--- blogGrupo/proyecto/blog/manage.py
+++ blogGrupo/proyecto/blog/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Rxdxdun administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings.local')
     try:
         from django.core.management import execute_from_command_line
